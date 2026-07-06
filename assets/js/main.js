af370279d82084c4ae04edf941453db68a8b0c50
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-07-06 13:07:27 +0600

    Fixed typed speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,8 +99,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 50,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
